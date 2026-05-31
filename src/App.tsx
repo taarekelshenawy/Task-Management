1f021cc73e773fc54eb0d6b1c8ed56ef4339b0ef
tarek <tarekelshenawy1997@gmail.com>
2026-05-31 13:59:57 +0300

    chore:@Install Tailwind CSS & Configure Project Variables

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-    <h1>hello</h1>
+    <h1 className="text-primary">hello</h1>
     </>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#003D9B',
+      },
+    },
+  },
+  plugins: [],
+}
