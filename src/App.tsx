b59d740b9d7eee50723216f11df3de2a908307d8
tarek <tarekelshenawy1997@gmail.com>
2026-06-05 16:59:16 +0300

    fix:add vite-supabase-url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import Routes from "./routes/routes"
+import Routes from "./routes/routes";
+import { useEffect } from "react";
 
 function App() {
-  
+  useEffect(() => {
+  console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
+}, []);
 
   return (
     <>
